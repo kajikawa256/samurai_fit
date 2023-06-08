deb30aec00f51383784f114673340071068107c3
matsui <2220372@ecc.ac.jp>
2023-06-08 10:11:08 +0900

    java to kotlin

--- app/src/main/java/com/example/samurai_fit/Setting.kt
+++ app/src/main/java/com/example/samurai_fit/Setting.kt
@@ -1,14 +1,11 @@
-package com.example.samurai_fit;
+package com.example.samurai_fit
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-import android.os.Bundle;
-
-public class Setting extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+class Setting : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
     }
 }
