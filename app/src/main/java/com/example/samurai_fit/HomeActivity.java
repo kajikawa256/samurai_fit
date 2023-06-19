c0ec7f9137114d4913a919ab2b07811528a15907
2220054 <2220054@ecc.ac.jp>
2023-06-19 09:40:34 +0900

    homeactivity作成中

--- app/src/main/java/com/example/samurai_fit/HomeActivity.java
+++ app/src/main/java/com/example/samurai_fit/HomeActivity.java
@@ -12,21 +12,6 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-
-        //Imageviewのpadding設定
-        ImageView imageView = findViewById(R.id.imageView);
-        imageView.setPadding(0,0,0,0);
-
-
-        //線の座標を判定
-        float startX = 100; // 線の開始X座標
-        float startY = 100; //線の開始Y座標
-        float endX = 300; //線の終了X座標
-        float endY = 500; //線の終了Y座標
-
-        customView.setLineCoordinates(startX, startY, endX, endY);
     }
 }
 
