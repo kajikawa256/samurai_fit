324b0a30a53b273ed59a08550ee66f800cc329cb
2220054 <2220054@ecc.ac.jp>
2023-06-22 12:23:37 +0900

    homeactivity作成中

--- app/src/main/java/com/example/samurai_fit/HomeActivity.kt
+++ app/src/main/java/com/example/samurai_fit/HomeActivity.kt
@@ -1,21 +1,37 @@
 package com.example.samurai_fit
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.content.Intent
+import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var weekTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        // 画面切り替え用のスイッチの処理
+        weekTextView = findViewById(R.id.weekview)
+        val myImageButton = findViewById<ImageButton>(R.id.button5)
+
+
+        val calendar = Calendar.getInstance()
+        val weekNumber = calendar.get(Calendar.WEEK_OF_YEAR)
+
+        val weekText = "週数: $weekNumber"
+        weekTextView.text = weekText
+
+        // 画面切り替え用のボタンの処理
         val myButton1 = findViewById<Button>(R.id.button1)
         val myButton2 = findViewById<Button>(R.id.button2)
         val myButton3 = findViewById<Button>(R.id.button3)
         val myButton4 = findViewById<Button>(R.id.button4)
-
 
         myButton1.setOnClickListener { }
         myButton2.setOnClickListener {
@@ -31,5 +47,11 @@ class HomeActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        myImageButton.setOnClickListener {
+            // ボタンがクリックされた時の処理をここに記述
+            Toast.makeText(applicationContext, "Toastメッセージ", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
